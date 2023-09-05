347e2e6a8a9a07c280ee5e138ae57e1b2842c0e7
matheusazambuja <matheusazambuja35@gmail.com>
2023-09-05 12:56:39 -0300

    feat(home): create home module

---
+++ src/app/modules/home/home.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeContainerComponent } from './containers/home-container/home-container.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomeContainerComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeModule { }
