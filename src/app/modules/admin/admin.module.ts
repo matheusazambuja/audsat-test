20a1f8ed9f6daa6325629f44c08fa189adb47fff
matheusazambuja <matheusazambuja35@gmail.com>
2023-09-05 12:56:23 -0300

    feat(admin): create admin module

---
+++ src/app/modules/admin/admin.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminContainerComponent } from './containers/admin-container/admin-container.component';
+
+@NgModule({
+  declarations: [AdminContainerComponent],
+  imports: [CommonModule],
+})
+export class AdminModule {}
