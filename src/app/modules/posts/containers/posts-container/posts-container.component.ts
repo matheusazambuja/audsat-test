09bd6b87a221025c8f051e6dbc3a335104d74740
matheusazambuja <matheusazambuja35@gmail.com>
2023-09-05 12:57:10 -0300

    feat(posts): add posts module

---
+++ src/app/modules/posts/containers/posts-container/posts-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-posts-container',
+  templateUrl: './posts-container.component.html',
+  styleUrls: ['./posts-container.component.scss']
+})
+export class PostsContainerComponent {
+
+}
---
+++ src/app/modules/posts/posts.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PostsContainerComponent } from './containers/posts-container/posts-container.component';
+
+
+
+@NgModule({
+  declarations: [
+    PostsContainerComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PostsModule { }
